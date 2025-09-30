2c36602ac1eeb60880ebada48de6fe59ff21c6c0
Hilal Özkan <Hilalozkan47@icloud.com>
2025-09-30 13:35:09 +0200

    Add project files.

---
+++ Wpf, pokemon demo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Wpf__pokemon_demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
